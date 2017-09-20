4569a5721a615374b12ee99e021cf124047c6788
Darko Lukic <lukicdarkoo@gmail.com>
2017-09-20 03:07:04 +0200

    Add execution time comparison of GA and HEFT

--- mlscheduler/usecases/ComparisonUseCase.py
+++ mlscheduler/usecases/ComparisonUseCase.py
@@ -4,9 +4,18 @@ from schedulers.ga.GAScheduler import GAScheduler
 from schedulers.TaskDuplicator import TaskDuplicator
 from schedulers.HEFTScheduler import HEFTScheduler
 import matplotlib.pyplot as plt
+import datetime
 
 
 class ComparisonUseCase(object):
+    @staticmethod
+    def time_diff(start, end):
+        diff = end - start
+        millis = diff.days * 24 * 60 * 60 * 1000
+        millis += diff.seconds * 1000
+        millis += diff.microseconds / 1000
+        return millis
+
     @staticmethod
     def run():
         print("Running: " + ComparisonUseCase.__name__)
@@ -15,23 +24,38 @@ class ComparisonUseCase(object):
 
         heft_times = []
         ga_times = []
+        heft_executions = []
+        ga_executions = []
         heft_costs = []
         ga_costs = []
         number_of_tasks = []
         for i in range(8, 30, 4):
             task_graph = RandomImporter.get_task_graph(n_tasks=i)
             task_graph.set_task_duplicator(task_duplicator=TaskDuplicator(w=w))
-            ga_scheduler = GAScheduler(task_graph=task_graph.copy(), nind=90, max_terminate=80, no_change_terminate=15, w=w)
+            ga_scheduler = GAScheduler(task_graph=task_graph.copy(), nind=20, max_terminate=20, no_change_terminate=5, w=w)
             heft_scheduler = HEFTScheduler(task_graph=task_graph.copy())
 
+            heft_start = datetime.datetime.now()
             heft_graph = heft_scheduler.calculate()
+            heft_end = datetime.datetime.now()
+
+            ga_start = datetime.datetime.now()
             ga_graph = ga_scheduler.calculate()
+            ga_end = datetime.datetime.now()
 
             number_of_tasks.append(i)
             heft_times.append(heft_graph.get_total_time())
             ga_times.append(ga_graph.get_total_time())
             heft_costs.append(heft_graph.get_total_cost())
             ga_costs.append(ga_graph.get_total_cost())
+            heft_executions.append(ComparisonUseCase.time_diff(heft_start, heft_end))
+            ga_executions.append(ComparisonUseCase.time_diff(ga_start, ga_end))
+
+        plt.plot(number_of_tasks, heft_executions, 'r')
+        plt.plot(number_of_tasks, ga_executions, 'b')
+        plt.ylabel('Execution Time')
+        plt.xlabel('Number of tasks')
+        plt.show()
 
         plt.plot(number_of_tasks, heft_times, 'r')
         plt.plot(number_of_tasks, ga_times, 'b')
